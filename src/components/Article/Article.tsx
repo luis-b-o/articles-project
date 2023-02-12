50df69fe05bbc416386591a8bb4e63c56e275f62
Luis Oliveira <luis.oliveira@startse.com>
2023-02-12 15:14:01 -0300

    tags list

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,17 +1,29 @@
 export type ArticleProps = {
   title: string;
   text: string[];
-  tags: string;
+  tags: string[];
   image: string;
   alt: string;
 };
 
 const Article = ({ title, text, tags, image, alt }: ArticleProps) => {
   return (
-    <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex flex-col items-center">
+    <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex gap-2 flex-col items-center">
       <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold">
         {title}
       </h3>
+
+      <div className="w-full flex-row justify-end gap-2 pr-5 hidden sm:flex">
+        {tags.map((tag) => (
+          <span
+            className="bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 font-bold text-xs uppercase"
+            key={tag}
+          >
+            {tag}
+          </span>
+        ))}
+      </div>
+
       <div className="grid gap-1">
         {text.map((content, index) => (
           <p className="text-alura-200 dark:text-gray-400" key={index}>
@@ -19,6 +31,7 @@ const Article = ({ title, text, tags, image, alt }: ArticleProps) => {
           </p>
         ))}
       </div>
+
       {image && <img className="sm:p-4" src={image} />}
       {image && alt && <span className="sr-only">{alt}</span>}
     </div>
